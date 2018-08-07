8c76214a3b026062db464354ce367610e8808c92
Ricardo Beas <rbeas@trovedata.com>
2018-08-06 22:34:15 -0400

    Favicon fix attempt 2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function(defaults) {
     },
     favicons: {
       faviconsConfig: {
-        path: '/rbeas-portfolio',
+        path: '',
         theme_color: '#304FFE'
       }
     }
